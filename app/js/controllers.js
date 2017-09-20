a51e17ba0c12f0d41bf836031114bdd25c87acfb
Mamithi <19lawrence93@gmail.com>
2017-09-20 08:32:53 +0300

    Binding model with controllers

---
+++ app/js/controllers.js
@@ -0,0 +1,7 @@
+angular.module('myApp.controllers', []).controller('FinanceController', function($scope){
+	$scope.salary = 0;
+	$scope.percentage = 0;
+	$scope.result = function(){
+		return $scope.salary*$scope.percentage*0.01;
+	};
+});
