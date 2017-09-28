88eba2de5119f8659831e524efe74d511439be79
Mamithi <19lawrence93@gmail.com>
2017-09-28 13:43:28 +0300

    Understanding about watchers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,4 +7,14 @@ angular.module('myApp.controllers', []).controller('SiteController', function($s
 angular.module('myApp.controllers').controller('BookController', function($scope){
 	$scope.books = ['Jump Start HTML5', 'Jump Start CSS', 'Jump Start Responsive Web Design'];
 	$scope.name = "Scope for BookController";
+	$scope.addToWishList = function(book){
+		$scope.wishListCount++;
+	};
+	$scope.wishListCount = 0;
+	$scope.$watch('wishListCount', function(newValue, oldValue){
+		console.log('called' + newValue + 'times');
+		if(newValue == 2){
+			alert('Great! You have 2 items in your wish list. Time to buy what you love. ')
+		}
+	});
 });
