01a9fba521a1989cd16a693b423956f081090868
Mamithi <19lawrence93@gmail.com>
2017-09-29 12:14:53 +0300

    Understand Angulars Js Events

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,4 @@
-'use strict';
-
 angular.module('myApp', [
 		'myApp.controllers'
-	]);
+]);
 
-angular.module('myApp').run(function($rootScope){
-	$rootScope.title = 'Famous Books';
-	$rootScope.name = "Root Scope";
-})
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,20 +1,43 @@
-angular.module('myApp.controllers', []).controller('SiteController', function($scope){
-	$scope.publisher = 'SitePoint';
-	$scope.type = "Web Development";
-	$scope.name = "Scope for SiteController";
+angular.module('myApp.controllers', []);
+
+angular.module('myApp.controllers').controller('MessageController', function($scope, $timeout){
+	$scope.messages = [{
+		sender: 'user1',
+		text: 'Message1'
+	}];
+
+	var timer;
+	var count = 0;
+
+	$scope.loadMessages = function(){
+		count++;
+		$scope.messages.push({
+			sender: 'user1',
+			text: 'Random Message' + count
+		});
+		timer = $timeout($scope.loadMessages, 2000);
+
+		if(count == 3){
+			$scope.$broadcast('EVENT_NO_DATA', 'Not Connected');
+			$timeout.cancel(timer);
+		}
+	};
+
+	timer = $timeout($scope.loadMessages, 2000);
+	$scope.$on('EVENT_RECEIVED', function(){
+		console.log('Receieved emmited event');
+	});
 });
 
-angular.module('myApp.controllers').controller('BookController', function($scope){
-	$scope.books = ['Jump Start HTML5', 'Jump Start CSS', 'Jump Start Responsive Web Design'];
-	$scope.name = "Scope for BookController";
-	$scope.addToWishList = function(book){
-		$scope.wishListCount++;
-	};
-	$scope.wishListCount = 0;
-	$scope.$watch('wishListCount', function(newValue, oldValue){
-		console.log('called' + newValue + 'times');
-		if(newValue == 2){
-			alert('Great! You have 2 items in your wish list. Time to buy what you love. ')
-		}
-	});
+angular.module('myApp.controllers').controller('StatsController', function($scope){
+	$scope.name = 'World';
+	$scope.status = 'Connected';
+	$scope.statusColor = 'green';
+
+	$scope.$on('EVENT_NO_DATA', function(event, data){
+		console.log('Receieved broadcasted event');
+		$scope.status = data;
+		$scope.statusColor = 'red';
+		$scope.$emit('EVENT_RECEIVED');
+	})
 });
