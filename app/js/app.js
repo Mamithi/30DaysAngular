df68269b1f101e8c70cb073e76b01884ca118bf4
Mamithi <19lawrence93@gmail.com>
2017-10-01 22:26:59 +0300

    Understand Angular JS routing

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,19 @@
-angular.module('myApp', [
-		'myApp.controllers'
-]);
+'use strict';
 
+angular.module('myApp', ['myApp.controllers', 'ngRoute']);
+
+angular.module('myApp').config(function($routeProvider, $locationProvider){
+	$routeProvider.when('/view1', {
+		controller: 'Controller1',
+		templateUrl: 'partials/view1.html'
+	}).when('/view2', {
+		controller: 'Controller2',
+		templateUrl: 'partials/view2.html'
+	});
+	$locationProvider.html5Mode({
+		enabled: true,
+		requireBase: false
+	});
+
+
+});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,43 +1,7 @@
-angular.module('myApp.controllers', []);
+'use strict';
 
-angular.module('myApp.controllers').controller('MessageController', function($scope, $timeout){
-	$scope.messages = [{
-		sender: 'user1',
-		text: 'Message1'
-	}];
-
-	var timer;
-	var count = 0;
-
-	$scope.loadMessages = function(){
-		count++;
-		$scope.messages.push({
-			sender: 'user1',
-			text: 'Random Message' + count
-		});
-		timer = $timeout($scope.loadMessages, 2000);
-
-		if(count == 3){
-			$scope.$broadcast('EVENT_NO_DATA', 'Not Connected');
-			$timeout.cancel(timer);
-		}
-	};
-
-	timer = $timeout($scope.loadMessages, 2000);
-	$scope.$on('EVENT_RECEIVED', function(){
-		console.log('Receieved emmited event');
-	});
-});
-
-angular.module('myApp.controllers').controller('StatsController', function($scope){
-	$scope.name = 'World';
-	$scope.status = 'Connected';
-	$scope.statusColor = 'green';
-
-	$scope.$on('EVENT_NO_DATA', function(event, data){
-		console.log('Receieved broadcasted event');
-		$scope.status = data;
-		$scope.statusColor = 'red';
-		$scope.$emit('EVENT_RECEIVED');
-	})
+angular.module('myApp.controllers', []).controller('Controller1', function($scope){
+	$scope.message = "Hello World";
+}).controller('Controller2', function($scope){
+	$scope.now = new Date();
 });
