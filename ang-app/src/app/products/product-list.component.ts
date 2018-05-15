744250f7989ec4bcf3ad387218b841e939bb0f60
Mamithi <lawrence@tospay.net>
2018-05-15 22:56:21 +0300

    Module id for relative paths

--- ang-app/src/app/products/product-list.component.ts
+++ ang-app/src/app/products/product-list.component.ts
@@ -4,6 +4,7 @@ import { IProduct} from './product';
 
 @Component({
   selector: 'pm-products',
+  moduleId: module.id,
   templateUrl: 'product-list.component.html',
   styleUrls: ['product-list.component.css']
 })
