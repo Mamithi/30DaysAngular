36d795b974846d4ebfba88ae0b32c71e0dde9cb6
Mamithi <lawrence@tospay.net>
2018-05-19 23:14:50 +0300

    Starting a second lesson on angular

---
+++ lesson2/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>Hello World</h1>
+    <my-tutorials></my-tutorials>  
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+}
---
+++ lesson2/src/app/tutorial/tutorial.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-tutorials',
+    templateUrl: 'tutorial.component.html',
+    styleUrls: ['tutorial.component.css']
+    })
+
+export class TutorialComponent {
+
+}
