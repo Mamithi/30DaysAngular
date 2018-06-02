3f688fcdb69b0986ce7cb301776d03a177e30b5b
Mamithi <19lawrence93@gmail.com>
2018-06-03 00:20:49 +0300

    First lesson on routing

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+
+const ROUTES: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'blog', component: BlogComponent},
+  {path: 'about', component: AboutComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
