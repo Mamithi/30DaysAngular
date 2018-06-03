8bb71b5b903f585f544f3dfddd852cd1d2dcdd29
Mamithi <19lawrence93@gmail.com>
2018-06-03 15:35:55 +0300

    Will be back soon teh teacher has decided to be vague

---
+++ music-search-routing/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import {
+  LocationStrategy,
+  HashLocationStrategy,
+  APP_BASE_HREF
+} from '@angular/common';
+
+import { AppComponent } from './app.component';
+import { AlbumComponent} from './album/album.component';
+import { ArtistComponent } from './artist/artist.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
