5ca90ca93ca45d86cff58329213e194ac1521461
Mamithi <19lawrence93@gmail.com>
2018-06-05 14:33:49 +0300

    First lesson on http client

--- http1/src/app/app.component.ts
+++ http1/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Post } from './post';
 // import { Observable } from 'rxjs/Observable';
 import { Observable } from 'rxjs';
@@ -15,10 +15,25 @@ export class AppComponent {
   ROOT_URL = 'https://jsonplaceholder.typicode.com';
 
   posts: Observable<any>;
+  newPost: Observable<any>;
 
   constructor(private http: HttpClient) { }
 
   getPosts() {
-    this.posts = this.http.get(this.ROOT_URL + '/posts?userId=1');
+    let params = new HttpParams().set('userId', '5');
+
+    this.posts = this.http.get(this.ROOT_URL + '/posts', { params });
+  }
+
+  createPost() {
+    const data: Post = {
+      id: null,
+      userId: 23,
+      title: 'My New Post',
+      body: 'Hello World!'
+
+    }
+
+    this.newPost = this.http.post(this.ROOT_URL + '/posts', data)
   }
 }
