1ae241fc7265f7954b068dafbad0edb3d34e4bd5
Mamithi <19lawrence93@gmail.com>
2018-07-03 00:46:46 +0300

    Validating form

--- blog/src/app/pages/admin/children/register/register.component.ts
+++ blog/src/app/pages/admin/children/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../../../models/user';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,10 +8,21 @@ import { User } from '../../../../models/user';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  registrationForm;
   constructor() { }
 
   ngOnInit() {
+    this.registrationForm = new FormGroup({
+      'firstname': new FormControl(this.user.firstname, [
+        Validators.required,
+        Validators.minLength(4),
+        // forbiddenNameValidator(/bob/i)
+      ])
+    });
+  }
+
+  get firstname(){
+    return this.registrationForm.get('firstname');
   }
 
   user = new User('John',
